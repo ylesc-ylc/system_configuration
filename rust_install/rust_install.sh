7c6fb992afa880ad6c2544b26b33fe7e01527ba3
ylesc <ylesc1217@gmail>
2024-03-21 20:57:59 +0800

    1

--- rust_install/rust_install.sh
+++ rust_install/rust_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo 'export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup'  >> .zshrc
-echo 'export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup'  >> .bashrc
+export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup
 
 wget https://github.com/ylesc-ylc/system_configuration/blob/main/rust_install/rustup-init.sh | sh
 
