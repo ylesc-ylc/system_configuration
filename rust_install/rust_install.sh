90ba4ec35052d8e743bf71f1b7aa85a9f425d1cd
ylesc <ylesc1217@gmail>
2024-03-21 20:35:08 +0800

    1

--- rust_install/rust_install.sh
+++ rust_install/rust_install.sh
@@ -6,7 +6,7 @@ if test -e ./zshrc
         echo "export RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup" >> ~/.bashrc
 fi
 
-wget https://gitee.com/ylesc/system_configuration/blob/main/rust_install/rustup-init.sh | sh
+wget https://github.com/ylesc-ylc/system_configuration/blob/main/rust_install/rustup-init.sh | sh
 
 echo "RUSTUP_DIST_SERVER=https://mirrors.tuna.tsinghua.edu.cn/rustup"  >> ~/.cargo/env  
 
